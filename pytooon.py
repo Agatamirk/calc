e727598ec4638020ce15519781073257677bca56
Agatamirk <agatamirk@gmail.com>
2018-02-03 21:19:02 +0100

    initial commit

---
+++ pytooon.py
@@ -0,0 +1,22 @@
+
+
+a = """My first program, try it:"""
+c = "do you want to count more???"
+print (a)
+loop = 1
+choice = 0
+while loop == 1:
+    x=int(input("type in first number: "))
+    y=int(input("type in second number: "))
+    z=input("Choose your operand, type +,-,* or / : ")
+    if z == "+":
+        print ("result is: ",(x+y))
+    elif z == "*":
+        print ("result is: ",(x*y))
+    elif z == "-":
+        print ("result is: ",(x - y))
+    elif z == "/":
+        print ("result is: ",(x / y))
+    else:
+        print ("wrong data!!!")
+    print (c)
